19b0c2fe51b8363170f03c5490e1d93ad3f45e04
luckponte <luckpontecorreia@gmail.com>
2021-11-13 12:03:12 -0300

    Create components for the main comic pages

--- app/src/app/comics/comics.module.ts
+++ app/src/app/comics/comics.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListComponent, DetailComponent],
   imports: [
     CommonModule
   ]
