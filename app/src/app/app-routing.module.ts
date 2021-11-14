66d95472aef0dcfd5872b2123373f54dd5e2c323
luckponte <luckpontecorreia@gmail.com>
2021-11-14 11:11:06 -0300

    Configure basic routing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailComponent } from './comics/detail/detail.component';
+import { ListComponent } from './comics/list/list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: ListComponent,
+  },
+  {
+    path: 'comic',
+    component: DetailComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
